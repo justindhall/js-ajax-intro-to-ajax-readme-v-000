801a16068c2bea17fff3dbc73c42b6e9976a9d13
Justin Hall <hall.justin.d@gmail.com>
2019-01-14 17:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,5 +31,7 @@ function getCommits(el){
 function showCommits(){
   const commits = JSON.parse(this.responseText);
   
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - '
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
+  
+  document.getElementById('commits').innerHTML = commitsList;
 }
