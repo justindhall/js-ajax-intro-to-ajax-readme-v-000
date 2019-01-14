0348fe426b1cb3e26959b4980e470736dafe28df
Justin Hall <hall.justin.d@gmail.com>
2019-01-14 17:42:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function showRepositories(){
   
   console.log(repos);
   
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name +'</li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo=" ' + r.name + '"onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   
   document.getElementById('repositories').innerHTML = repoList;
 }
