9a7436ac96f026185aa45fe4c98b6bcedd469d09
Justin Hall <hall.justin.d@gmail.com>
2019-01-14 17:44:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ function showRepositories(){
   
   document.getElementById('repositories').innerHTML = repoList;
 }
+
+function getCommits(el){
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  
+  req.addEventListener('load', showCommits);
+  req.open('GET','https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
